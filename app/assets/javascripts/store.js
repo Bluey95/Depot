5bdf1ed6b86c55ca40d87a8b34880cd80845e079
Bluey95 <susanwere15@gmail.com>
2018-07-30 01:15:23 +0300

    uglifier

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,5 +1,3 @@
-$(document).on "ready page:change", -> 
-console.log('ready or page change') 
 $(".store .entry > h3").not(".clickable").addClass("clickable").click -> 
 console.log('image clicked') 
 $(this).parent().find(':submit').click()
